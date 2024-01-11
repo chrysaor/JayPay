d676c7818b1a4777b34d9cc97e5910ec32ffa39d
jaewookim <chrysaor@naver.com>
2024-01-12 01:39:26 +0900

    feat: add some files for kafka connection (#12)

---
+++ banking-service/src/main/java/com/jaypay/banking/BankingConfig.java
@@ -0,0 +1,11 @@
+package com.jaypay.banking;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.jaypay.common")
+public class BankingConfig {
+
+}
