e88e4ae6441d1e5cb9350706ff90c12029957b57
jaewookim <chrysaor@naver.com>
2024-01-12 01:39:26 +0900

    feat: add some files for kafka connection (#12)

---
+++ membership-service/src/main/java/com/jaypay/membership/MembershipConfig.java
@@ -0,0 +1,11 @@
+package com.jaypay.membership;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.jaypay.common")
+public class MembershipConfig {
+    
+}
---
+++ money-service/src/main/java/com/jaypay/money/MoneyConfig.java
@@ -0,0 +1,11 @@
+package com.jaypay.money;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.jaypay.common")
+public class MoneyConfig {
+    
+}
