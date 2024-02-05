7689b11bdecdac8aa8ac4cb1f79151b253661341
jaewookim <chrysaor@naver.com>
2024-02-06 00:07:13 +0900

    feat: add money aggregation service (#25)

---
+++ money-aggregation-service/src/main/java/com/jaypay/money/aggregation/adapter/in/web/GetMoneySumController.java
@@ -0,0 +1,24 @@
+package com.jaypay.money.aggregation.adapter.in.web;
+
+import com.jaypay.common.WebAdapter;
+import com.jaypay.money.aggregation.application.port.in.GetMoneySumByAddressCommand;
+import com.jaypay.money.aggregation.application.port.in.GetMoneySumByAddressUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@WebAdapter
+@RestController
+@RequiredArgsConstructor
+public class GetMoneySumController {
+    private final GetMoneySumByAddressUseCase getMoneySumByAddressUseCase;
+    @PostMapping(path = "/money/aggregation/get-money-sum-by-address")
+    int getMoneySumByAddress(@RequestBody GetMoneySumByAddressRequest request) {
+
+        return getMoneySumByAddressUseCase.getMoneySumByAddress(
+                GetMoneySumByAddressCommand.builder()
+                        .address(request.getAddress()).build()
+        );
+    }
+}
